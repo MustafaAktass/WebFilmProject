81b5ef3e006593ede16cf782100462724b36d3b7
MustafaAktass <gokberkmustafa@outlook.com>
2024-06-10 08:30:53 +0300

    sitemap eklendi

--- controller/admin/userVisitController.js
+++ controller/admin/userVisitController.js
@@ -1,9 +1,13 @@
 const UserVisit = require('../../models/userVisitModel');
 
-
 module.exports.getTotalVisits = async (req, res) => {
-  const totalVisits = await UserVisit.countDocuments();
-  res.json({ totalVisits });
+  try {
+    const totalVisits = await UserVisit.countDocuments();
+    res.json({ totalVisits });
+  } catch (error) {
+    console.error('Error fetching total visits:', error);
+    res.status(500).json({ error: 'Failed to fetch total visits' });
+  }
 };
 
 module.exports.getOnlineUsers = (req, res) => {
--- middlewares/trackVisitsMiddleware.js
+++ middlewares/trackVisitsMiddleware.js
@@ -1,8 +1,9 @@
 const UserVisit = require('../models/userVisitModel');
+const requestIp = require('request-ip');
 
 const trackVisits = async (req, res, next) => {
-  if (req.path.startsWith('/user')) { // Admin sayfası ziyaretlerini izleme
-    const ip = req.ip;
+  if (req.path.startsWith('/user')) { // Sadece /user yollarını izleme
+    const ip = requestIp.getClientIp(req); // IP adresini alın
     const existingVisit = await UserVisit.findOne({ ip: ip });
     if (!existingVisit) {
       const userVisit = new UserVisit({ ip: ip });
@@ -12,6 +13,7 @@ const trackVisits = async (req, res, next) => {
   next();
 };
 
+module.exports = trackVisits;
 //ip adresi filtrelemesi yapmadan toplam kullanıcı sayısı hesaplama
 // const trackVisits = async (req, res, next) => {
 //     if (req.path.startsWith('/admin')) { // Admin sayfası ziyaretlerini izleme
@@ -21,4 +23,3 @@ const trackVisits = async (req, res, next) => {
 //     next();
 //   };
 
-module.exports = trackVisits;
